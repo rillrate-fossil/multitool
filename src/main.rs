ea7ba850fcfb971c14b8f5d2613887cb7c684271
Denis Kolodin <deniskolodin@gmail.com>
2021-08-08 23:40:55 +0300

    Add total cpu info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("It's in the progress...");
+use anyhow::Error;
+use rillrate::PulseFrameTracer;
+use std::thread;
+use std::time::Duration;
+use sysinfo::{ProcessorExt, System, SystemExt};
+
+const PKG: &str = "Heroic Toys";
+const DSHB_SYSTEM: &str = "System Monitor";
+const GRP_CPUS: &str = "CPUs";
+
+fn main() -> Result<(), Error> {
+    env_logger::try_init()?;
+    let _handle = rillrate::start();
+    let cpu_total = PulseFrameTracer::new([PKG, DSHB_SYSTEM, GRP_CPUS, "CPU [total]"].into(), None);
+    let mut system = System::new_all();
+    loop {
+        system.refresh_cpu();
+        let usage = system.global_processor_info().cpu_usage();
+        cpu_total.add(usage);
+        thread::sleep(Duration::from_millis(500));
+    }
 }
