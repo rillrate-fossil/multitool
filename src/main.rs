ca5a2fa4297ead013676817e7c9216437c56fa1e
Denis Kolodin <deniskolodin@gmail.com>
2021-08-09 16:28:44 +0300

    Add boards with system parameters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Error;
+use rillrate::board::BoardListTracer;
 use rillrate::pulse::{Label, PulseFrameSpec, PulseFrameTracer, Range};
 use std::collections::HashMap;
 use std::thread;
@@ -11,6 +12,7 @@ const DSHB_SYSTEM: &str = "System Monitor";
 const DSHB_CPU: &str = "CPU Monitor";
 
 const GRP_LOAD: &str = "System Load";
+const GRP_INFO: &str = "System Info";
 const GRP_CPUS: &str = "CPUs";
 
 fn main() -> Result<(), Error> {
@@ -19,6 +21,28 @@ fn main() -> Result<(), Error> {
     let _handle = rillrate::start();
 
     let mut system = System::new_all();
+
+    let cpu_board = BoardListTracer::new([PKG, DSHB_SYSTEM, GRP_INFO, "CPU Info"].into());
+    let proc = system.global_processor_info();
+    cpu_board.set("Frequency", format!("{} MHz", proc.frequency()));
+    cpu_board.set("Name", proc.name());
+    cpu_board.set("Vendor ID", proc.vendor_id());
+    cpu_board.set("Brand", proc.brand());
+
+    let info_board = BoardListTracer::new([PKG, DSHB_SYSTEM, GRP_INFO, "System Info"].into());
+    info_board.set("Name", system.name().unwrap_or_default());
+    info_board.set(
+        "Kernel Version",
+        system.kernel_version().unwrap_or_default(),
+    );
+    info_board.set("Host Name", system.host_name().unwrap_or_default());
+    info_board.set("OS Version", system.os_version().unwrap_or_default());
+    info_board.set(
+        "OS Version (long)",
+        system.long_os_version().unwrap_or_default(),
+    );
+    info_board.set("Total Memory", system.total_memory());
+    info_board.set("Total Swap", system.total_swap());
 
     let cpu_spec = Some(PulseFrameSpec {
         retain: 30,
