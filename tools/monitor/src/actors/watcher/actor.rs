dad8c8f978ca4bae57ba9191ac94a2473f9e52e3
Denis Kolodin <deniskolodin@gmail.com>
2021-09-26 17:43:04 +0300

    Use fields instead of cells

--- tools/monitor/src/actors/watcher/actor.rs
+++ tools/monitor/src/actors/watcher/actor.rs
@@ -8,15 +8,9 @@ use anyhow::Error;
 use async_trait::async_trait;
 use meio::task::HeartBeatHandle;
 use meio::{Actor, Context, InterruptedBy, StartedBy};
-use once_cell::sync::OnceCell;
 use rillrate::prime::*;
 use std::sync::Arc;
 use std::time::Duration;
-
-// TODO: Use local fields instead
-static LATENCY: OnceCell<String> = OnceCell::new();
-static TAIL: OnceCell<String> = OnceCell::new();
-static INTERVAL: OnceCell<String> = OnceCell::new();
 
 pub struct Watcher {
     name: String,
@@ -25,6 +19,10 @@ pub struct Watcher {
     tail: LiveTail,
     interval: Slider,
     handle: HeartBeatHandle,
+
+    latency_entry: String,
+    tail_entry: String,
+    interval_entry: String,
 }
 
 impl Actor for Watcher {
@@ -33,9 +31,9 @@ impl Actor for Watcher {
 
 impl Watcher {
     pub fn new(name: String, url: String) -> Self {
-        LATENCY.set(format!("site.{}.latency.pulse", name)).unwrap();
+        let latency_entry = format!("site.{}.latency.pulse", name);
         let latency = Pulse::new(
-            LATENCY.get().unwrap().as_ref(),
+            latency_entry.as_ref(),
             Default::default(),
             PulseOpts::default()
                 .retain(30u32)
@@ -44,17 +42,15 @@ impl Watcher {
                 .higher(true)
                 .suffix("ms"),
         );
-        TAIL.set(format!("site.{}.latency.events", name)).unwrap();
+        let tail_entry = format!("site.{}.latency.events", name);
         let tail = LiveTail::new(
-            TAIL.get().unwrap().as_ref(),
+            tail_entry.as_ref(),
             Default::default(),
             LiveTailOpts::default(),
         );
-        INTERVAL
-            .set(format!("site.{}.settings.interval", name))
-            .unwrap();
+        let interval_entry = format!("site.{}.settings.interval", name);
         let interval = Slider::new(
-            INTERVAL.get().unwrap().as_ref(),
+            interval_entry.as_ref(),
             SliderOpts::default()
                 .label("Slide Me!")
                 .min(1)
@@ -71,6 +67,9 @@ impl Watcher {
             tail,
             interval,
             handle,
+            latency_entry,
+            tail_entry,
+            interval_entry,
         }
     }
 }
--- tools/monitor/src/actors/watcher/actor/layout.rs
+++ tools/monitor/src/actors/watcher/actor/layout.rs
@@ -3,11 +3,11 @@ use rillrate::basis::{Layout, LayoutTab};
 
 impl Watcher {
     pub fn register_layout(&mut self) {
-        let mut layout = Layout::new(self.name.as_ref());
-        let mut tab = LayoutTab::new("Latency");
-        tab.add_item((0, 0), (20, 30), LATENCY.get().unwrap().as_ref());
-        tab.add_item((0, 30), (20, 30), TAIL.get().unwrap().as_ref());
-        tab.add_item((0, 60), (20, 30), INTERVAL.get().unwrap().as_ref());
+        let mut layout = Layout::new("Monitor");
+        let mut tab = LayoutTab::new(self.name.as_ref());
+        tab.add_item((0, 0), (60, 40), self.tail_entry.as_ref());
+        tab.add_item((60, 0), (40, 40), self.latency_entry.as_ref());
+        tab.add_item((0, 60), (20, 30), self.interval_entry.as_ref());
         layout.add_tab(tab);
         layout.register();
     }
