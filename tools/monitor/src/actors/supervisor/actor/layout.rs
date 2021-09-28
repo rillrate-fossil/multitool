9d943a988e1946b6dfff221d23dde407539a6d10
Denis Kolodin <deniskolodin@gmail.com>
2021-09-28 22:13:39 +0300

    Use improved layout

--- tools/monitor/src/actors/supervisor/actor/layout.rs
+++ tools/monitor/src/actors/supervisor/actor/layout.rs
@@ -1,15 +1,13 @@
 use super::*;
-use rillrate::basis::{Layout, LayoutTab};
+use rillrate::basis::Layout;
 
 impl Supervisor {
     pub fn register_layout(&mut self) {
-        let mut layout = Layout::new("Global");
-        let mut tab = LayoutTab::new("Settings");
+        let mut tab = Layout::new(["Global", "Settings"]);
         tab.add_item((0, 0), (20, 30), INPUT_NAME);
         tab.add_item((0, 30), (20, 30), INPUT_URL);
         tab.add_item((0, 60), (20, 30), BUTTON_ADD);
-        layout.add_tab(tab);
-        layout.register();
+        tab.register();
     }
 
     pub fn unregister_layout(&mut self) {}
--- tools/monitor/src/actors/watcher/actor/layout.rs
+++ tools/monitor/src/actors/watcher/actor/layout.rs
@@ -1,15 +1,13 @@
 use super::*;
-use rillrate::basis::{Layout, LayoutTab};
+use rillrate::basis::Layout;
 
 impl Watcher {
     pub fn register_layout(&mut self) {
-        let mut layout = Layout::new("Monitor");
-        let mut tab = LayoutTab::new(self.name.as_ref());
+        let mut tab = Layout::new(["Monitor", self.name.as_ref()]);
         tab.add_item((0, 0), (60, 40), self.tail_entry.as_ref());
         tab.add_item((60, 0), (40, 40), self.latency_entry.as_ref());
         tab.add_item((0, 60), (20, 30), self.interval_entry.as_ref());
-        layout.add_tab(tab);
-        layout.register();
+        tab.register();
     }
 
     pub fn unregister_layout(&mut self) {}
