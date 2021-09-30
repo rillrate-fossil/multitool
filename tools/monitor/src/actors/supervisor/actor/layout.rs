877fd1b6f0c19aed9e14c215f1f5b6449830d893
Denis Kolodin <deniskolodin@gmail.com>
2021-09-30 18:01:12 +0300

    Add Expanded and Spacer

--- tools/monitor/src/actors/supervisor/actor/layout.rs
+++ tools/monitor/src/actors/supervisor/actor/layout.rs
@@ -4,12 +4,18 @@ use rillrate::basis::*;
 impl Supervisor {
     pub fn register_layout(&mut self) {
         let mut tab = Layout::new(["Global", "Settings"]);
-        tab.set_container(Row::new(vec![Column::new(vec![
-            Flow::new(INPUT_NAME).into(),
-            Flow::new(INPUT_URL).into(),
-            Row::new(vec![Flow::new(BUTTON_ADD).into()]).into(),
-        ])
-        .into()]));
+        tab.set_container(Row::new(vec![
+            Expanded::new(
+                Column::new(vec![
+                    Flow::new(INPUT_NAME).into(),
+                    Flow::new(INPUT_URL).into(),
+                    Row::new(vec![Flow::new(BUTTON_ADD).into()]).into(),
+                ]),
+                1.0,
+            )
+            .into(),
+            Expanded::new(Spacer::new(), 3.0).into(),
+        ]));
         tab.register();
     }
 
